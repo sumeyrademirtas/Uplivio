aca878e8b29a512862b3dd059319345383d4c233
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-10-15 15:32:34 +0300

    Initial Commit

---
+++ Uplivio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Uplivio
+//
+//  Created by Sümeyra Demirtaş on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
